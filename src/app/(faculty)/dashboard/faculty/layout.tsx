d93b92bef9a814bf69795de9704129056c7b94a8
Manu BG <bgmanu2426@gmail.com>
2025-09-23 15:34:16 +0000

    Fic the header issue still its not fixed when i scroll the header is get

--- src/app/(faculty)/dashboard/faculty/layout.tsx
+++ src/app/(faculty)/dashboard/faculty/layout.tsx
@@ -11,7 +11,7 @@ export default function FacultyDashboardLayout({
     <SidebarProvider>
       <FacultySidebar />
       <SidebarInset>
-        <div className="flex flex-col h-screen overflow-hidden">
+        <div className="flex flex-col h-screen">
           <Header />
           <main className="flex-1 overflow-y-auto p-4 sm:p-6">{children}</main>
         </div>
